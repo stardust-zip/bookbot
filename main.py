bd5aabbe72659a5c86bc868427497a1b0933b966
stardust-zip <nguyenhieu0802@proton.me>
2025-10-25 08:11:05 +0700

    feat: finish bookbot blueprint

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from stats import get_book_text, count_words, count_characters
+import sys
+from stats import (
+    count_characters,
+    count_words,
+    get_book_text,
+    sorted_dict,
+)
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
-    book_text_count = count_words(book_text)
-    print(f"Found {book_text_count} total words")
-    print(count_characters(book_text))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
+    words_count = count_words(book_text)
+    char_dict = count_characters(book_text)
+    dict_list = sorted_dict(char_dict)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    for d in dict_list:
+        if d["char"].isalpha():
+            print(f"{d['char']}: {d['num']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -10,11 +10,33 @@ def count_words(book_text):
 
 def count_characters(book_text):
     char_dict = {}
-
     for character in book_text.lower():
         if character not in char_dict:
             char_dict[character] = 1
         else:
             char_dict[character] += 1
-
     return char_dict
+
+
+def count_specific_word(book_text, word_to_find):
+    count = 0
+    for word in book_text.lower().split():
+        if word_to_find == word:
+            count += 1
+    return count
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sorted_dict(char_dict):
+    dict_list = []
+    for char, amount in char_dict.items():
+        dict_list.append({"char": char, "num": amount})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+
+def useless_function():
+    return "You are a failure"
